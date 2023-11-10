b86748f0031690b872da821524a0b4e6cf2f849c
ricqCodes <princenwakanma1996@gmail.com>
2023-11-10 12:57:30 +0100

    removed unused imports

--- src/pages/app/_molecules/Timer.tsx
+++ src/pages/app/_molecules/Timer.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { styled } from "styled-components";
-import { slideInDown } from "../../../styles/animate.styled";
 
 interface IProps {
   timerNum: number;
--- src/pages/app/phase/_molecules/PhaseHeader.tsx
+++ src/pages/app/phase/_molecules/PhaseHeader.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { styled } from "styled-components";
 import Timer from "../../_molecules/Timer";
 
